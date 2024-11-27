d4e429fc90617ac829d4207e120ce0616f547ef4
ahnyongjae <ahnyongjae@gmail.com>
2024-11-27 18:10:25 +0900

    application.yml을 github의 secrets.APPLICATION_PROPERTIES로 추가

--- src/main/java/com/example/github_actions_practice/AppController.java
+++ src/main/java/com/example/github_actions_practice/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String home() {
-        return "Hello World!!! 하이 안용제!!! CI/CD 적용됨!!! SSH 연동이 되지 않아 고생하고 있음!!! SSH 연동은 된 듯!!! gradlew 실행권한 없음!!! 이번에는 문법 오류 으~~~ 이번에 Hang!!! Swap Memory 증설 중(t2.micro환경 swap메모리 할당으로 Linux 가상 메모리 늘려주기)!!! application.properties -> application.yml";
+        return "Hello World!!! 하이 안용제!!! CI/CD 적용됨!!! SSH 연동이 되지 않아 고생하고 있음!!! SSH 연동은 된 듯!!! gradlew 실행권한 없음!!! 이번에는 문법 오류 으~~~ 이번에 Hang!!! Swap Memory 증설 중(t2.micro환경 swap메모리 할당으로 Linux 가상 메모리 늘려주기)!!! application.properties -> application.yml!!! application.yml을 github의 secrets.APPLICATION_PROPERTIES로 추가!!!";
     }
 }
