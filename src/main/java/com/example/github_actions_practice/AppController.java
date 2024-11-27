9449562545fe38fadc44ffbd9356ec4b3ce54669
ahnyongjae <ahnyongjae@gmail.com>
2024-11-27 11:58:18 +0900

    github actions 적용 9

--- src/main/java/com/example/github_actions_practice/AppController.java
+++ src/main/java/com/example/github_actions_practice/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String home() {
-        return "Hello World!!! 하이 안용제!!! CI/CD 적용됨!!! SSH 연동이 되지 않아 고생하고 있음!!! SSH 연동은 된 듯. memory 오류 발생!!! Swap을 늘리고 다시 시도";
+        return "Hello World!!! 하이 안용제!!! CI/CD 적용됨!!! SSH 연동이 되지 않아 고생하고 있음!!! SSH 연동은 된 듯. timeout 오류 발생!!! Swap을 늘리고 다시 시도";
     }
 }
